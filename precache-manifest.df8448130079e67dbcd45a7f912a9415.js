af2d8465d86b57e579941704438c14d0b550a306
pasha29 <pasha4383@gmail.com>
2020-05-11 05:08:36 +0300

    Updates

---
+++ precache-manifest.df8448130079e67dbcd45a7f912a9415.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b1a6cd0936118b4365b653c52689d0b",
+    "url": "/taskabz/index.html"
+  },
+  {
+    "revision": "2a12f6d9f960fbcef302",
+    "url": "/taskabz/static/css/main.95b5d89a.chunk.css"
+  },
+  {
+    "revision": "26b06120bc67f26202a4",
+    "url": "/taskabz/static/js/2.b4fdc612.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/taskabz/static/js/2.b4fdc612.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a12f6d9f960fbcef302",
+    "url": "/taskabz/static/js/main.15fbfdd9.chunk.js"
+  },
+  {
+    "revision": "b5d89bdfa2f01580ef8c",
+    "url": "/taskabz/static/js/runtime-main.b8f2216c.js"
+  },
+  {
+    "revision": "f5443d74025f2a6069e0ca0142c08a05",
+    "url": "/taskabz/static/media/banner-photo.f5443d74.jpg"
+  },
+  {
+    "revision": "146e29265bcc680129e263a9478dca00",
+    "url": "/taskabz/static/media/logo.146e2926.svg"
+  },
+  {
+    "revision": "3cdf78971c869b16b95152be7b6922e5",
+    "url": "/taskabz/static/media/man-laptop-v1.3cdf7897.svg"
+  }
+]);
